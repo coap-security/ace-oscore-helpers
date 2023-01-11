dd4f26994ce629e2a808950f7f08e10be5c16a6e
chrysn <chrysn@fsfe.org>
2023-01-11 23:29:13 +0100

    Documentation fixes

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,15 @@ pub mod request_creation_hints;
 pub use aead;
 pub use aes;
 
-// from RFC9203
 // FIXME: I'd rather provide encoding and decoding structs for payloads around /authz-info, but the
 // CBOR encoder situation doesn't support the general writers yet.
+/// OAuth Parameters CBOR Mapping value for access_token (RFC9200 Section 8.10)
 pub const ACCESS_TOKEN: u64 = 1;
+/// OAuth Parameters CBOR Mapping value for nonce1 (RFC9203 Section 9.3)
 pub const NONCE1: u64 = 40;
+/// OAuth Parameters CBOR Mapping value for nonce2 (RFC9203 Section 9.3)
 pub const NONCE2: u64 = 42;
+/// OAuth Parameters CBOR Mapping value for ace_client_recipientid (RFC9203 Section 9.3)
 pub const ACE_CLIENT_RECIPIENTID: u64 = 43;
+/// OAuth Parameters CBOR Mapping value for ace_server_recipientid (RFC9203 Section 9.3)
 pub const ACE_SERVER_RECIPIENTID: u64 = 44;
